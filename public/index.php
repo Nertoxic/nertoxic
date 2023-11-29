893b01bb426b566defc28b3ff9494dd1e0ddc430
Robin <101270933+DevelopingFlakes@users.noreply.github.com>
2023-11-29 15:08:44 +0100

    Start

--- public/index.php
+++ public/index.php
@@ -1,1 +1,16 @@
-dd
+<?php 
+#
+#  _   _ _____ ____ _____ _____  _____ ____ 
+# | \ | | ____|  _ \_   _/ _ \ \/ /_ _/ ___|
+# |  \| |  _| | |_) || || | | \  / | | |    
+# | |\  | |___|  _ < | || |_| /  \ | | |___ 
+# |_| \_|_____|_| \_\|_| \___/_/\_\___\____|
+#
+#
+
+try {
+    include 'app/nic/display/error_content_default.html'; 
+} catch (Exception $e) {
+    print_r("There was a fatal error while loading the error page. Please contact the system administrator to fix the routing.");
+}
+?>
