175702e0ff56fb09bec6b215af5e1999380cc0a5
Robin <101270933+DevelopingFlakes@users.noreply.github.com>
2023-11-29 21:21:48 +0100

    Start

--- nic/core/nicEnvLoader.php
+++ nic/core/nicEnvLoader.php
@@ -11,7 +11,7 @@
 if ($env === false) {
 
     include BASE_PATH.'app/nic/display/error_env_not_loaded.html';
-    debug_to_console("The .env file, inside the BASE_PATH, couldnt be loaded. Please check the file for errors");
+    echo "<script>console.log('Debug Objects: .env couldnt be loaded' );</script>";
     die();
 
 } else {
