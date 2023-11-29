454c46185c5a00e481e5351034a6192142a4162e
Robin <101270933+DevelopingFlakes@users.noreply.github.com>
2023-11-29 21:33:58 +0100

    Start

--- nicLoader.php
+++ nicLoader.php
@@ -10,12 +10,13 @@
 
 define('BASE_PATH', __DIR__.'/');
 
+try {
 
 # --------------------------------------------------------------------
 # These files are core file assists, if these files doesnt work some of the core files wont even start working
 #--------------------------------------------------------------------
 
-include BASE_PATH.'nicEnv.php'; # Load Env variables
+#include BASE_PATH.'nicEnv.php'; # Load Env variables
 include BASE_PATH.'nicVersion.php'; # Load NIC Version
 
 # --------------------------------------------------------------------
@@ -28,4 +29,24 @@ include BASE_PATH.'nic/visitor/nicVisLoc.php'; # Load visitor tracking module [h
 # These files are basic files which adding some cool features to the framework
 # --------------------------------------------------------------------
 
+} 
+
+
+# --------------------------------------------------------------------
+# Error handling
+# --------------------------------------------------------------------
+
+catch (Exception $e) {
+
+    include BASE_PATH.'app/nic/display/error_frame_loading.html';
+    echo "<script>console.log('[NERTOXIC] There was an error while loading the nicLoader.php, please check it for errors and check if every file is at the correct path' );</script>";
+    die();
+
+}
+
+if($nic_version == NULL) {
+    include BASE_PATH.'app/nic/display/error_frame_loading.html';
+    echo "<script>console.log('[NERTOXIC] There was an error while reading the file nicVersion.php, please check if the file is placed at the base_path' );</script>";
+    die();
+}
 ?>
