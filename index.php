21b30ea692d3c999b532f0752c4dd322f740b7a5
Robin <101270933+DevelopingFlakes@users.noreply.github.com>
2023-11-29 14:59:13 +0100

    Start

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-<?php include 'app/nic/display/error_public_path.html'; ?>
+<?php 
+try {
+    include 'app/nic/display/error_public_path.html'; 
+} catch (Exception $e) {
+    print_r("There was a fatal error while loading the error page. Please contact the system administrator to fix the routing.");
+}
+?>
