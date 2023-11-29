61461dc9ec289fb5e38388ce07febc1ef8d897b3
Robin <101270933+DevelopingFlakes@users.noreply.github.com>
2023-11-29 15:01:27 +0100

    Start

--- nicLoader.php
+++ nicLoader.php
@@ -1,7 +1,26 @@
 <?php
+#
+#  _   _ _____ ____ _____ _____  _____ ____ 
+# | \ | | ____|  _ \_   _/ _ \ \/ /_ _/ ___|
+# |  \| |  _| | |_) || || | | \  / | | |    
+# | |\  | |___|  _ < | || |_| /  \ | | |___ 
+# |_| \_|_____|_| \_\|_| \___/_/\_\___\____|
+#
+#
+
+try {
+
 // nicLoader
 define('BASE_PATH', __DIR__.'/');
 
 // Start loading files from app
 include BASE_PATH.'app/controller/db/load.php';
+
+} 
+
+catch (Exception $e) {
+
+    include BASE_PATH.'app/nic/display/error_frame_loading.html';
+
+}
 ?>
