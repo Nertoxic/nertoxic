c2385658651a90208fdc80f39b2a3453a10d2a47
Robin <101270933+DevelopingFlakes@users.noreply.github.com>
2023-11-29 14:55:38 +0100

    Start

--- index.php
+++ index.php
@@ -20,14 +20,15 @@
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex items-center pt-8 sm:justify-start sm:pt-0">
-                    <div class="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
+                    <h1 class="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
                         NIC
-                    </div>
+                    </h1>
 
                     <div class="ml-4 text-lg text-gray-500 uppercase tracking-wider">
                         ERROR
                     </div>
                 </div>
+                <hr>
                 <center>
                 <small class="px-4 text-lg text-gray-500 border-gray-400 align-center">
                     There was an setup error, please contact the system administrator to change the default path to /public/
