03d15a59f15802bea045c5b242071980f82d6932
Robin <101270933+DevelopingFlakes@users.noreply.github.com>
2023-11-29 14:52:06 +0100

    Start

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
                         ERROR
                     </div>
                 </div>
-                <small>hey</small>
+                <small class="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">hey</small>
             </div>
         </div>
     </body>
