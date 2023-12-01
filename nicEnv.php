22a2a16c4fc3df97867df93b65bbafa4102520e9
Robin <101270933+DevelopingFlakes@users.noreply.github.com>
2023-12-01 12:38:48 +0100

    Start

--- nicEnv.php
+++ nicEnv.php
@@ -11,25 +11,4 @@
 $env = parse_ini_file('.env');
 include BASE_PATH.'nic/core/nicEnvLoader.php';
 
-if ($env === false) {
-
-    $nicConsoleErrorFile = ".env";
-    $nicConsoleErrorCritical = "true";
-    $nicConsoleErrorDesc = "The .env couldnt be loaded, please check it for any errors and check your server console";
-    include BASE_PATH.'nic/core/nicConsole.php';
-
-} else {
-
-    $NIC_LICENSE = $env['NIC_LICENSE'];
-    $NIC_ENV_TYPE = $env['NIC_ENV_TYPE'];
-    $NIC_BASE_URI = $env['NIC_BASE_URL'];
-
-    $NIC_USED_DB = $env['NIC_USED_DB'];
-
-    $NIC_MYSQL_HOST = $env['NIC_MYSQL_HOST'];
-    $NIC_MYSQL_DB_NAME = $env['NIC_MYSQL_DB_NAME'];
-    $NIC_MYSQL_USER_NAME = $env['NIC_MYSQL_USER_NAME'];
-    $NIC_MYSQL_USER_PASS = $env['NIC_MYSQL_USER_PASS'];
-
-}
 ?>
