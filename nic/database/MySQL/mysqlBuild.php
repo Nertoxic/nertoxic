dfb307c89c25e7da5e6c3b33efb760410537e61b
Robin <101270933+DevelopingFlakes@users.noreply.github.com>
2023-12-01 12:28:24 +0100

    Start

--- nic/database/MySQL/mysqlBuild.php
+++ nic/database/MySQL/mysqlBuild.php
@@ -7,18 +7,18 @@ class mysql
     public function db()
     {
 
+        /*
         $dbHost = $GLOBALS['NIC_MYSQL_HOST'];
         $dbName = $GLOBALS['NIC_MYSQL_DB_NAME'];
         $dbUser = $GLOBALS['NIC_MYSQL_USER_NAME'];
         $dbPass = $GLOBALS['NIC_MYSQL_USER_PASS'];
+        */
 
         // TESTING
-        /*
         $dbHost = 'localhost';
         $dbName = 'nicTestDB';
         $dbUser = 'nicTestDB_user';
         $dbPass = '#n562hMb3';
-        */
 
         $db = new PDO('mysql:host=' . $dbHost . ';charset=utf8;dbname=' . $dbName, $dbUser, $dbPass);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
