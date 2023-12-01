8ea10277eca736202fd7b4f21d4b0d675ab6a94a
Robin <101270933+DevelopingFlakes@users.noreply.github.com>
2023-12-01 12:31:52 +0100

    Start

---
+++ public/test/database.php
@@ -0,0 +1,19 @@
+<?php
+#
+#  _   _ _____ ____ _____ _____  _____ ____ 
+# | \ | | ____|  _ \_   _/ _ \ \/ /_ _/ ___|
+# |  \| |  _| | |_) || || | | \  / | | |    
+# | |\  | |___|  _ < | || |_| /  \ | | |___ 
+# |_| \_|_____|_| \_\|_| \___/_/\_\___\____|
+#
+#
+$nicPageType = "x";
+include '../../nicLoader.php';
+?>
+
+<?php
+    $DB_TEST = $mysql->db()->prepare("SELECT * FROM `nicTesting`");
+    $DB_TEST->execute();
+    while ($testDB = $DB_TEST -> fetch(PDO::FETCH_ASSOC)){
+    }
+?>
