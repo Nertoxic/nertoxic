21ca232957f9af53bb14e3551f0c6a713d8f0ad4
Robin <101270933+DevelopingFlakes@users.noreply.github.com>
2023-12-01 12:52:10 +0100

    Start

--- nic/core/nicConsole.php
+++ nic/core/nicConsole.php
@@ -12,11 +12,11 @@
 
 echo "<script>console.log('--------------------- [NERTOXIC] ---------------------');</script>";
 echo "<script>console.log('');</script>";
-echo "<script>console.log('File: ".$nicConsoleErrorFile."');</script>";
-echo "<script>console.log('Ciritcal: ".$nicConsoleErrorCritical."');</script>";
+echo "<script>console.error('File: ".$nicConsoleErrorFile."');</script>";
+echo "<script>console.error('Ciritcal: ".$nicConsoleErrorCritical."');</script>";
 echo "<script>console.error('Error: ".$nicConsoleErrorDesc."');</script>";
 echo "<script>console.log('');</script>";
-echo "<script>console.debug('--------------------- [NERTOXIC] ---------------------');</script>";
+echo "<script>console.log('--------------------- [NERTOXIC] ---------------------');</script>";
 
 if($nicConsoleErrorCritical == "true") {
     include BASE_PATH.'nic/out/error.html';
