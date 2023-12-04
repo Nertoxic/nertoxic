8db39270460354f7af80369fbe1f4152adaef85e
Robin <101270933+DevelopingFlakes@users.noreply.github.com>
2023-12-04 13:50:35 +0100

    core: added function randomeString to generate a randome string with given length

--- nic/core/nicFunctions.php
+++ nic/core/nicFunctions.php
@@ -28,11 +28,44 @@ class nicFun
         $licenseKey = $GLOBALS['NIC_LICENSE'];
         //return $licenseKey; // Disabled for Security reasons, could be used to grab license keys
     }
-
+    
+    /*
+     * checkLicense
+     *
+     * This function will either return true = valid or
+     * false = not valid
+     */
     public function checkLicense()
     {
         $licenseKey = $GLOBALS['NIC_LICENSE'];
         return true;
+    }
+    
+    /*
+     * randomeString
+     *
+     * This function will generate a randome String, with
+     * the option to set the length of the randome string
+     */
+    public function randomeString($length, $special)
+    {
+        // If special chars allowed
+        if($special == true) {
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"§$%&/()=?*+#´`';
+        } else {
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        }
+
+        // Set the Char length
+        $charactersLength = strlen($characters);
+        $randomString = '';
+
+        // Generate the string
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+
+        return $randomString;
     }
 
 }
