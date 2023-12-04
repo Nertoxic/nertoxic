321a06285d5991bb7779bf2636d0ef1f0de4e9b4
Robin <101270933+DevelopingFlakes@users.noreply.github.com>
2023-12-04 15:21:19 +0100

    modules: Added nicDate module to set a default date value and give it a php variable called $date

---
+++ nic/modules/nicDate/index.php
@@ -0,0 +1,22 @@
+<?php 
+#
+#  _   _ _____ ____ _____ _____  _____ ____ 
+# | \ | | ____|  _ \_   _/ _ \ \/ /_ _/ ___|
+# |  \| |  _| | |_) || || | | \  / | | |    
+# | |\  | |___|  _ < | || |_| /  \ | | |___ 
+# |_| \_|_____|_| \_\|_| \___/_/\_\___\____|
+#
+#
+
+# --------------------------------------------------------------------
+# Module: nicDate
+# Creator: Nertoxic (DevelopingFlakes)
+# Desc: This will handle some Date specific requests
+# --------------------------------------------------------------------
+
+$moduleIsActive = false;
+
+if($moduleIsActive == true) {
+    include MODULE_PATH.'nicDate/code.php';
+}
+?>
