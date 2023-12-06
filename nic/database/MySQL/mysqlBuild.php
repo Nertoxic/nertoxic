c06d5b75ba49a7f846da261446300807606291eb
Robin <101270933+DevelopingFlakes@users.noreply.github.com>
2023-12-06 22:55:36 +0100

    Removed comments from mysql builder

--- nic/database/MySQL/mysqlBuild.php
+++ nic/database/MySQL/mysqlBuild.php
@@ -12,14 +12,6 @@ class mysql
         $dbUser = $GLOBALS['NIC_MYSQL_USER_NAME'];
         $dbPass = $GLOBALS['NIC_MYSQL_USER_PASS'];
 
-        // Official test db, usable by anyone who thinks the db isnt working right. The user doesnt got permissions to delete or edit anything
-        /*
-        $dbHost = 'localhost';
-        $dbName = 'nicTestDB';
-        $dbUser = 'nicTestDB_user';
-        $dbPass = '#n562hMb3';
-        */
-
         $db = new PDO('mysql:host=' . $dbHost . ';charset=utf8;dbname=' . $dbName, $dbUser, $dbPass);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $db;
