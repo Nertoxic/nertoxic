a5c29712d436a44a66767abb8ad1c91bf04874cc
Robin <101270933+DevelopingFlakes@users.noreply.github.com>
2023-12-12 12:01:28 +0100

    Trying to make the geoplugin run #4

--- public/example/geoplugin.php
+++ public/example/geoplugin.php
@@ -14,4 +14,21 @@ include '../../nicLoader.php';
 <?php 
     $geoplugin = new geoPlugin();
     $geoplugin->locate(); 
+    echo "Geolocation results for {$geoplugin->ip}: <br />\n".
+	"City: {$geoplugin->city} <br />\n".
+	"Region: {$geoplugin->region} <br />\n".
+	"Region Code: {$geoplugin->regionCode} <br />\n".
+	"Region Name: {$geoplugin->regionName} <br />\n".
+	"DMA Code: {$geoplugin->dmaCode} <br />\n".
+	"Country Name: {$geoplugin->countryName} <br />\n".
+	"Country Code: {$geoplugin->countryCode} <br />\n".
+	"In the EU?: {$geoplugin->inEU} <br />\n".
+	"EU VAT Rate: {$geoplugin->euVATrate} <br />\n".
+	"Latitude: {$geoplugin->latitude} <br />\n".
+	"Longitude: {$geoplugin->longitude} <br />\n".
+	"Radius of Accuracy (Miles): {$geoplugin->locationAccuracyRadius} <br />\n".
+	"Timezone: {$geoplugin->timezone}  <br />\n".
+	"Currency Code: {$geoplugin->currencyCode} <br />\n".
+	"Currency Symbol: {$geoplugin->currencySymbol} <br />\n".
+	"Exchange Rate: {$geoplugin->currencyConverter} <br />\n";
 ?>
