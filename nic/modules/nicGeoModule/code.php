849fae2f0766984ca800a5a93efcfbae9651303b
Robin <101270933+DevelopingFlakes@users.noreply.github.com>
2023-12-12 11:52:29 +0100

    Trying to use json_decode #4

--- nic/modules/nicGeoModule/code.php
+++ nic/modules/nicGeoModule/code.php
@@ -9,6 +9,6 @@
 #
 
 
-$clientGeo_pl = json_encode(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$clientAddr));
+$clientGeo_pl = json_decode(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$clientAddr));
 $clientGeo = $clientGeo_pl[0]['geoplugin_request'];
 var_dump($clientGeo);
