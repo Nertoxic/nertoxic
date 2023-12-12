a7833a2886c6fd847abf96cae7d57d8ccd3b17be
Robin <101270933+DevelopingFlakes@users.noreply.github.com>
2023-12-12 15:08:58 +0100

    Still no error found, returning to stable commits

--- nicLoader.php
+++ nicLoader.php
@@ -39,8 +39,8 @@ require BASE_PATH.'nicVersion.php'; # Load NIC Version
 require BASE_PATH.'vendor/autoload.php'; # Loa all vendor files
 
 // Cache System
-require CORE_PATH.'nicCache.php'; # Load all cache files
-require CACHE_PATH.'load.php'; # Load all cache files
+//require CORE_PATH.'nicCache.php'; # Load all cache files
+//require CACHE_PATH.'load.php'; # Load all cache files
 
 // Database loading
 if($env['NIC_USED_DB'] == "mysql") {
