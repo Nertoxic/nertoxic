55ef8ddf6400b7c76eec63bedbed9ba876f91232
Robin <101270933+DevelopingFlakes@users.noreply.github.com>
2023-12-13 08:38:36 +0100

    Fixed an error inside the header rederict of the mail verify system

--- nic/handler/nicPageNameHandler.php
+++ nic/handler/nicPageNameHandler.php
@@ -28,8 +28,8 @@ if(!$sessionToken == NULL) {
         // nicAuth Mail Verify
         if($NIC_AUTH_FORCE_MAIL_VERIFY == 'true') {
             if($usermail_verified == 'false') {
-                echo($GET['re']);
-                if($GET['re'] == "yes") {} else {
+                echo($_GET['re']);
+                if($_GET['re'] == "yes") {} else {
                     echo("yeee");
                     //header("Location: ".$NIC_BASE_URL.$NIC_AUTH_MAIL_VERIFY_PAGE."?re=yes");
                 }
