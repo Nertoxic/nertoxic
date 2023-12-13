69e2f99080c8e597ec820a23382a4a1f505c5230
Robin <101270933+DevelopingFlakes@users.noreply.github.com>
2023-12-13 08:14:33 +0100

    Added debug values

--- public/example/dashboard/charge.php
+++ public/example/dashboard/charge.php
@@ -12,6 +12,8 @@ include '../../../nicLoader.php'; # Check if you used the correct loading folder
 
 if(isset($_POST['chargeMoney'])) {
     $inv->startPayment($_POST['amount'], $_POST['provider'], $userid);
+    echo($_POST['amount']);
+    echo($_POST['provider']);
 }
 ?>
 
