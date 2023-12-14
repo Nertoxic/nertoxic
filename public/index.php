530bb3a9a8bccf4ccd79c590daff2464d740d49e
Robin <101270933+DevelopingFlakes@users.noreply.github.com>
2023-12-14 10:24:41 +0100

    Removed specific error handling

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
 # Load the Framework
 # --------------------------------------------------------------------
 
-error_reporting(E_ALL ^ E_WARNING && E_NOTICE); // Set own error reporting
+#error_reporting(E_ALL ^ E_WARNING && E_NOTICE); // Set own error reporting
 include_once '../nicLoader.php';
 
 ?>
