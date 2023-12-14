81b5150eb600c1348f932026f8c45d2c04711dff
Robin <101270933+DevelopingFlakes@users.noreply.github.com>
2023-12-14 10:20:03 +0100

    Trying to fix post error

--- app/pages/example/index.php
+++ app/pages/example/index.php
@@ -8,8 +8,27 @@
 #
 #
 
-$currPage = 'front_index';
+$currPage = 'auth_login';
 require_once BASE_PATH.'nic/handler/nicPageHandler.php';
 ?>
 
-Welcome!
+
+<h1> Login Example Page </h1>
+<br>
+<small>
+    <?php
+    if(isset($_POST['login'])) {
+        $auth->login($_POST['username'], $_POST['password']);
+    }
+    ?>
+</small>
+
+<br><br>
+
+<form method="POST">
+<?= $_POST['username']; ?>
+<input name="username" placeholder="Username"></input> <br>
+<input name="password" placeholder="Password"></input> <br>
+
+<button name="login" type="submit">Login</button>
+</form>
