a9a592f9894a09c86ecdfe5f6483e2f1183ad9e3
Robin <101270933+DevelopingFlakes@users.noreply.github.com>
2023-12-14 11:18:42 +0100

    Trying to find out why user binded values not being displayed

--- nicLoader.php
+++ nicLoader.php
@@ -54,7 +54,7 @@ require CORE_PATH.'nicFunctions.php'; # Load the Security functions
 
 // Routing Stuff
 require BASE_PATH.'nicRouter.php'; # Load the router
-require BASE_PATH.'nic/handler/nicPageHandler.php';
+require HANDLER_PATH.'nicPageHandler.php';
 
 } catch (Exception $e) {
     $nicCon->callError(true, 'nicLoader.php', 'There was an error while loading the loader.');
